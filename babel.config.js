00295f2d016526f660368cdfdc161b9148a34ad9
slavapavlutin <sl.pavlutin@gmail.com>
2019-05-17 14:58:12 +0300

    Use envs in Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,52 @@
 module.exports = {
-  presets: ['next/babel', '@zeit/next-typescript/babel'],
+  env: {
+    development: {
+      presets: [
+        [
+          'next/babel',
+          {
+            'preset-env': {
+              targets: 'last 1 Chrome version',
+            },
+          },
+        ],
+        '@zeit/next-typescript/babel',
+      ],
+    },
+    production: {
+      presets: [
+        [
+          'next/babel',
+          {
+            'preset-env': {
+              targets: {
+                chrome: '65',
+                safari: '11.1',
+                firefox: '60',
+                edge: '17',
+              },
+            },
+          },
+        ],
+        '@zeit/next-typescript/babel',
+      ],
+    },
+    test: {
+      presets: [
+        [
+          'next/babel',
+          {
+            'preset-env': {
+              targets: {
+                node: 'current',
+              },
+            },
+          },
+        ],
+        '@zeit/next-typescript/babel',
+      ],
+    },
+  },
+
   plugins: ['babel-plugin-macros'],
 };
