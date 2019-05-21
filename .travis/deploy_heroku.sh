3840fa2f609afc23318ee5225068b154d2179cc7
slavapavlutin <sl.pavlutin@gmail.com>
2019-05-21 21:55:45 +0300

    Update deploy script

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,4 +2,4 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web
+heroku container:release web --app $HEROKU_APP_NAME
