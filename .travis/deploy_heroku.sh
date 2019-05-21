82a8ef4feb4e2c617b2c669ec0f3710fc748a1db
slavapavlutin <sl.pavlutin@gmail.com>
2019-05-22 00:48:39 +0300

    Update deploy scripts

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+heroku container:push web --app $HEROKU_APP_NAME --arg firebase_api_key=$FIREBASE_API_KEY,firebase_project_id=$FIREBASE_PROJECT_ID,firebase_auth_domain=$FIREBASE_AUTH_DOMAIN,firebase_database_url=$FIREBASE_DATABASE_URL,firebase_storage_bucket=$FIREBASE_STORAGE_BUCKET,firebase_messaging_sender_id=$FIREBASE_MESSAGING_SENDER_ID
 heroku container:release web --app $HEROKU_APP_NAME
---
+++ bin/docker_deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+heroku container:push web --arg firebase_api_key=$FIREBASE_API_KEY,firebase_project_id=$FIREBASE_PROJECT_ID,firebase_auth_domain=$FIREBASE_AUTH_DOMAIN,firebase_database_url=$FIREBASE_DATABASE_URL,firebase_storage_bucket=$FIREBASE_STORAGE_BUCKET,firebase_messaging_sender_id=$FIREBASE_MESSAGING_SENDER_ID
+heroku container:release web
